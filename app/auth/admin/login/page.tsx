5a713a54fe902c95b313c56f8a4c3aa00f515952
yasuda <samonkntd@gmail.com>
2025-08-06 11:39:50 +0900

    add/page: auth/admin

---
+++ app/auth/admin/login/page.tsx
@@ -0,0 +1,9 @@
+import { AdminLoginForm } from "@/components/admin/AdminLoginForm";
+
+export default function AdminLoginPage() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <AdminLoginForm />
+    </div>
+  );
+}
