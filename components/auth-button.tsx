d398f77ffa45894e6ee934dab22abef7afbd344a
yasuda <samonkntd@gmail.com>
2025-08-09 17:19:17 +0900

    add/fix: auth button

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -17,7 +17,7 @@ export async function AuthButton({ user: initialUser }: { user: User | null }) {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      {user.email}
       <LogoutButton />
     </div>
   ) : (
